d3ca533b0657b455961cd94974eafcbbd95aec91
user <kilavyk@yandex.ru>
2025-05-19 22:07:46 +0300

    fix saver.py

--- src/saver.py
+++ src/saver.py
@@ -12,15 +12,6 @@ def create_database(params: dict, db_name: str) -> None:
 
         # Формирование запроса
         with conn.cursor() as cur:
-
-            # Закрываем все активные соединения с БД
-            cur.execute(f"""
-                SELECT pg_terminate_backend(pg_stat_activity.pid)
-                FROM pg_stat_activity
-                WHERE pg_stat_activity.datname = '{db_name}'
-                  AND pid <> pg_backend_pid();
-            """)
-
             cur.execute(f"DROP DATABASE IF EXISTS {db_name}")  # Удаляем БД
             cur.execute(f"CREATE DATABASE {db_name}")  # Создаём БД
         print(f"База '{db_name}' успешно пересоздана!")
